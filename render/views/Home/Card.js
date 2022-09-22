7e5cd6b6d190411a226bee549f2b0ad8bf4723ab
Gelingjie <gelingjie@didiglobal.com>
2022-09-22 11:59:47 +0800

    perf: home

--- render/views/Home/Card.js
+++ render/views/Home/Card.js
@@ -2,21 +2,19 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 export default function Card(props) {
-  const {desc,
-      title,
-      img,
-      href
-    } = props
+  const { desc, title, img, href, index } = props
   return (
     <Link to={href}>
       <div className="card">
-        <img src={img} />
+        <img className='card-bg' src={require(`./assets/b${index}.png`)} />
         <div className="title">
           <span>
             {title}
           </span>
         </div>
+        
         <div className="descInfo">
+          <img className='card-icon' src={ require(`./assets/${index}.png`) } />
           <span className="desc">{desc}</span>
         </div>
       </div>
--- render/views/Home/index.js
+++ render/views/Home/index.js
@@ -1,58 +1,57 @@
 import React from 'react'
 import Card from './Card.js'
 import './index.less'
-import kobe from './assets/kobe.jpg'
 
 
 const template = [
   {
     title: 'BLOG',
     href: '/Blog',
-    img: kobe,
-    desc: '个人技术博客：主要记录一些平常开发中的一些记录',
+    
+    desc: '个人技术博客',
     key: '1',
   },
   {
     
     title: 'NIC-CLI',
-    desc: '自主开发的脚手架：根据平常的开发，生成了一套适合自己的开发模版',
+    desc: '自主开发的脚手架',
     href: '/NicCli',
-    img: kobe,
+    
     key: '2',
   },
   {
     title: 'ELECTRON',
-    desc: '记录一些electron中最简单的一些方法，也是在开发中可能遇到需要使用的方法',
-    img: kobe,
+    desc: '系统方法',
+    
     key: '3',
     href: '/Electron'
   },
   {
     title: '组件库',
-    desc: '收集了一些平常开发起来好用的组件库，以及好用的第三方库',
+    desc: '第三方库',
     key: '4',
-    img: kobe,
+    
     href: '/ComponentLib'
   },
   {
     title: 'nodeJS',
     desc: 'nodeJS相关知识',
     key: '6',
-    img: kobe,
+    
     href: '/NodeJS'
   },
   {
     title: 'React Native',
     desc: 'RN处理移动端跨端的问题',
     key: '7',
-    img: kobe,
+    
     href: '/ReactNative'
   },
   {
     title: 'Interface',
     desc: '接口文档',
     key: '8',
-    img: kobe,
+    
     href: '/Interface'
   }
 ]
@@ -61,7 +60,7 @@ export default function index() {
   return (
     <div className="Home">
       {
-        template.map(item => <Card {...item} />)
+        template.map((item, index) => <Card {...item} index={index} />)
       }
     </div>
   )
